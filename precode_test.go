9ffa2a621834f4ff08eb38e3dfc961d1e0df4170
it-2 <otumanov@gmail.com>
2023-12-27 08:21:02 +0300

    fix =)

--- precode_test.go
+++ precode_test.go
@@ -12,67 +12,44 @@ import (
 
 func TestMainHandlerEmptyCountParameter(t *testing.T) {
 	req, err := http.NewRequest(http.MethodGet, "/?city=moscow", nil)
-
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
+	responseRecorder := makeResponseRecorder(t, err, req)
 	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 	assert.Equal(t, "count missing", responseRecorder.Body.String())
 }
 
 func TestMainHandlerUnsupportedCities(t *testing.T) {
 	req, err := http.NewRequest(http.MethodGet, "/cafe?city=ivanovo&count=100500", nil)
-
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
+	responseRecorder := makeResponseRecorder(t, err, req)
 	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 	assert.Contains(t, responseRecorder.Body.String(), "wrong city value")
 }
 
 func TestMainHandlerCounterNotIsNumber(t *testing.T) {
 	req, err := http.NewRequest(http.MethodGet, "/?city=moscow&counter=абырвалг", nil)
-
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
+	responseRecorder := makeResponseRecorder(t, err, req)
 	require.Equal(t, http.StatusBadRequest, responseRecorder.Code)
 	assert.Equal(t, "count missing", responseRecorder.Body.String())
 }
 
 func TestMainHandlerCorrectRequest(t *testing.T) {
 	req, err := http.NewRequest(http.MethodGet, "/cafe?&city=moscow&count=2", nil)
-
-	require.NoError(t, err)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req)
-
+	responseRecorder := makeResponseRecorder(t, err, req)
 	require.Equal(t, http.StatusOK, responseRecorder.Code)
 	assert.NotEmpty(t, responseRecorder.Body.String())
 }
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	req, err := http.NewRequest(http.MethodGet, "/cafe?city=moscow&count=10", nil)
+	responseRecorder := makeResponseRecorder(t, err, req)
+	data, err := io.ReadAll(responseRecorder.Body)
+	require.Equal(t, responseRecorder.Code, http.StatusOK)
+	assert.Equal(t, len(strings.Split(string(data), ",")), len(cafeList["moscow"]))
+}
 
-	require.Nil(t, err)
-
+func makeResponseRecorder(t *testing.T, err error, req *http.Request) *httptest.ResponseRecorder {
+	require.NoError(t, err)
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
-
-	data, err := io.ReadAll(responseRecorder.Body)
-
-	require.Equal(t, responseRecorder.Code, http.StatusOK)
-	assert.Equal(t, len(strings.Split(string(data), ",")), len(cafeList["moscow"]))
+	return responseRecorder
 }
